3167e53799fe5bb0352bb6ceb54ae1aa4e5a9439
Valentin Giorgetti <minot0rdevelopement@gmail.com>
2018-05-26 17:17:47 +0200

    added a new animation called text shuffler

---
+++ text_shuffler/assets/Shuffler.js
@@ -0,0 +1,40 @@
+function Shuffler(basis, fadeIn, fadeOut, element, chars){
+    this.basis = basis.split('');
+    this.new = basis.split('');
+    this.fadeIn = fadeIn;
+    this.fadeOut = fadeOut;
+    this.element = element;
+    this.chars = chars;
+
+    this.shuffle = function(startupTime){
+        var index = Math.round(Math.random() * (this.basis.length - 1));
+        var random = this.chars[Math.round(Math.random() * (this.chars.length - 1))];
+        this.new[index] = random;
+        this.element.innerHTML = this.new.join('');
+        if(Date.now() - startupTime < this.fadeIn){
+            let that = this;
+            setTimeout(function() { that.shuffle(startupTime) }, 100);
+        }else{
+            this.unshuffle(this.createSequence(), this.basis, 0, this.element);
+        }
+    }
+
+    this.unshuffle = function(sequence, basis, index, element){
+        if(index < sequence.length){
+            this.element.innerHTML = this.element.textContent.substr(0, sequence[index]) + basis[sequence[index]] + this.element.textContent.substr(sequence[index] + basis[sequence[index]].length);
+            let that = this;
+            setTimeout(function() { that.unshuffle(sequence, basis, index+1, element) }, this.fadeOut/sequence.length);
+        }
+    }
+
+    this.createSequence = function(){
+        var sequence = [];
+        for(var i = 0; i < this.basis.length; i++) { sequence.push(i); }
+        sequence = sequence.sort(function() { return 0.5 - Math.random() });
+        return sequence;
+    }
+
+}
+
+var s = new Shuffler('Text swapper algorithm designed by minot0r', 10000, 2000, document.getElementById('content'), '!<>-_\\/[]{}Ã¢â‚¬â€=+*^?#________'.split(''))
+s.shuffle(Date.now());
