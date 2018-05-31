bc0fbb4e05fa923c712537e35b8cad009db8dd7e
Valentin Giorgetti <minot0rdevelopement@gmail.com>
2018-05-31 20:01:16 +0200

    made the transitions between texts smoother

--- text_shuffler/assets/Shuffler.js
+++ text_shuffler/assets/Shuffler.js
@@ -1,17 +1,20 @@
 function Shuffler(fadeIn, fadeOut, element, chars){
     this.basis;
     this.new;
+    this.prev;
     this.fadeIn = fadeIn;
     this.fadeOut = fadeOut;
     this.element = element;
     this.chars = chars;
 
     this.shuffle = function(startupTime){
-        var index = Math.round(Math.random() * (this.basis.length - 1));
+        var elapsed = Date.now() - startupTime;
+        var len = Math.round(((elapsed-0)/(this.fadeIn-0))*(this.basis.length-this.prev.length)+this.prev.length);
+        var index = Math.round(Math.random() * len);
         var random = this.chars[Math.round(Math.random() * (this.chars.length - 1))];
         this.new[index] = random;
-        this.element.innerHTML = this.new.join('');
-        if(Date.now() - startupTime < this.fadeIn){
+        this.element.innerHTML = this.new.join('').substr(0, len);
+        if(elapsed < this.fadeIn){
             let that = this;
             setTimeout(function() { that.shuffle(startupTime) }, 50);
         }else{
@@ -34,10 +37,19 @@ function Shuffler(fadeIn, fadeOut, element, chars){
         return sequence;
     }
 
-    this.run = function(text){
-        this.basis = text.split('');
-        this.new = text.split('');
+    this.run = function(phrases, start){
+        this.prev = phrases[(start == 0) ? phrases.length-1 : start-1];
+        this.basis = phrases[start].split('');
+        this.new = this.element.textContent.split('');
         this.shuffle(Date.now());
+        let that = this;
+        setTimeout(function(){
+            if(start < phrases.length-1){
+                that.run(phrases, start+1);
+            }else{
+                that.run(phrases, 0);
+            }
+        }, this.fadeIn+this.fadeOut+1000);
     }
 
 }
@@ -46,18 +58,5 @@ function Shuffler(fadeIn, fadeOut, element, chars){
 window.onload = function(e){
     const phrases = ['minot0r', 'the', 'web', 'developer'];
     var s = new Shuffler(2000, 500, document.getElementById('content'), '!<>-_\\/[]{}Ã¢â‚¬â€=+*^?#________'.split(''))
-    loop(phrases, 0);
-
-    function loop(phrases, index) {
-        s.run(phrases[index]);
-        if(index < phrases.length-1){
-            setTimeout(function() {
-                loop(phrases, index+1);
-            }, 2000+500+1000);
-        }else{
-            setTimeout(function() {
-                loop(phrases, 0);
-            }, 2000+500+1000);
-        }
-    }
+    s.run(phrases, 0);
 }
